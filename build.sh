141673b90761b2d72bc72a7ae289b8ce205b2b19
melles1991 <melles.android@gmail.com>
2021-03-19 22:40:05 +0200

    defconfig: lavender: Regenerate
    Signed-off-by: melles1991 <melles.android@gmail.com>

--- build.sh
+++ build.sh
@@ -1,58 +1,106 @@
-#!/bin/bash
-#
-# Compile script for QuicksilveR kernel
-# Copyright (C) 2020-2021 Adithya R.
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
+# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
+# Android Kernel Build Script
 
-SECONDS=0 # builtin bash timer
-ZIPNAME="QuicksilveRV2-ginkgo-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="$HOME/tc/proton-clang"
-DEFCONFIG="vendor/ginkgo-perf_defconfig"
+echo -e " "
+echo -e " "
+echo -e "$blue░▐█▀█░▐█░▐█░▐██░▐█▀█▄▒▐█▀▀█▌▒▐█▀▀▄░▐██"
+echo -e "░▐█──░▐████─░█▌░▐█▌▐█▒▐█▄▒█▌▒▐█▒▐█─░█▌"
+echo -e "░▐█▄█░▐█░▐█░▐██░▐█▄█▀▒▐██▄█▌▒▐█▀▄▄░▐██$nocol"
+echo -e " "
 
-export PATH="$TC_DIR/bin:$PATH"
+#Set Color
+blue='\033[0;34m'
+grn='\033[0;32m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+txtbld=$(tput bold)
+txtrst=$(tput sgr0)      
 
-if ! [ -d "$TC_DIR" ]; then
-echo "Proton clang not found! Cloning to $TC_DIR..."
-if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $TC_DIR; then
-echo "Cloning failed! Aborting..."
-exit 1
+# Main environtment
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+ZIP_DIR=$KERNEL_DIR/AnyKernel3
+CONFIG=ginkgo-perf_defconfig
+
+# Export
+export ARCH=arm64
+export CROSS_COMPILE
+export CROSS_COMPILE_ARM32
+export KBUILD_BUILD_USER=melles1991
+export KBUILD_BUILD_HOST=CraftRom-build
+
+
+echo -e "${txtbld}Config:${txtrst} $CONFIG"
+echo -e "${txtbld}ARCH:${txtrst} $ARCH"
+echo -e "${txtbld}Username:${txtrst} $KBUILD_BUILD_USER"
+make mrproper
+if [  -d "./out/" ]; then
+        rm -rf ./out/
 fi
+
+# Main Staff
+
+clang_bin="$HOME/toolchains/proton-clang/bin"
+gcc_prefix64="aarch64-linux-gnu-"
+gcc_prefix32="arm-linux-gnueabi-"
+CROSS_COMPILE="aarch64-linux-gnu-"
+CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
+
+_ksetup_old_path="$PATH"
+export PATH="$clang_bin:$PATH"
+
+# Build start
+echo -e "$blue    \nMake DefConfig\n $nocol"
+make	O=out $CONFIG
+echo -e "$blue    \nBuild kernel\n $nocol"
+make	\
+	O=out \
+	ARCH=arm64 \
+	CC=clang \
+	LD=ld.lld \
+	AR=llvm-ar \
+	NM=llvm-nm \
+	STRIP=llvm-strip \
+	OBJCOPY=llvm-objcopy \
+	OBJDUMP=llvm-objdump \
+	CROSS_COMPILE=aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+	-j`nproc --all`
+
+if ! [ -a $KERN_IMG ]; then
+    echo -e "$red \nKernel Compilation failed! Fix the errors!\n $nocol"
 fi
 
-export KBUILD_BUILD_USER=adithya
-export KBUILD_BUILD_HOST=ghostrider_reborn
-
-mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
-
-if [[ $1 == "-r" || $1 == "--regen" ]]; then
-cp out/.config arch/arm64/configs/$DEFCONFIG
-echo -e "\nRegened defconfig succesfully!"
-exit 0
-else
-echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
-fi
-
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3; then
-echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
-exit 1
-fi
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cp out/arch/arm64/boot/dtbo.img AnyKernel3
-rm -f *zip
-cd AnyKernel3
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+cd $ZIP_DIR
+make clean &>/dev/null
 cd ..
-rm -rf AnyKernel3
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-if command -v gdrive &> /dev/null; then
-gdrive upload --share $ZIPNAME
-else
-echo "Zip: $ZIPNAME"
-fi
-rm -rf out/arch/arm64/boot
-else
-echo -e "\nCompilation failed!"
-fi
+
+# For MIUI Build
+# Credit Adek Maulana <adek@techdro.id>
+OUTDIR="$KERNEL_DIR/out/"
+VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel3/modules/vendor/lib/modules"
+
+STRIP="$HOME/toolchains/proton-clang/aarch64-linux-gnu/bin/strip$(echo "$(find "$HOME/toolchains/proton-clang/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+            sed -e 's/gcc/strip/')"
+for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
+    "${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
+    "${OUTDIR}"/scripts/sign-file sha512 \
+            "${OUTDIR}/certs/signing_key.pem" \
+            "${OUTDIR}/certs/signing_key.x509" \
+            "${MODULES}"
+    find "${OUTDIR}" -name '*.ko' -exec cp {} "${VENDOR_MODULEDIR}" \;
+done
+cd libufdt/src && python2 mkdtboimg.py create $OUTDIR/arch/arm64/boot/dtbo.img $OUTDIR/arch/arm64/boot/dts/qcom/*.dtbo
+echo -e "$grn    \n(i) Done moving modules\n $nocol"
+
+cd $ZIP_DIR
+cp $KERN_IMG zImage
+cp $OUTDIR/arch/arm64/boot/dtbo.img $ZIP_DIR
+make normal &>/dev/null
+echo -e "$blue    \nFlashable zip generated under $yellow$ZIP_DIR.\n $nocol"
+cd ..
+# Build end
